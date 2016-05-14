4e79b2495ebb599ddc4b0952bbcedc6ff92c237b
Kevin Attfield <k.attfield@gmail.com>
2016-05-14 16:08:55 -0700

    Add devise User
    rails generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :form_actions, path: 'forms', only: [:show] do
     resources :form_submissions, path: 's', only: [:create]
   end

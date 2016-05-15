e966e7f7e09a677d94da119fb5ca47c3d391e122
Kevin Attfield <k.attfield@gmail.com>
2016-05-14 18:13:20 -0700

    Show a logged in user their form actions

--- app/controllers/form_actions_controller.rb
+++ app/controllers/form_actions_controller.rb
@@ -1,4 +1,8 @@
 class FormActionsController < ApplicationController
+  def index
+    @form_actions = current_user.form_actions
+  end
+
   def show
     @form_action = FormAction.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :form_actions, path: 'forms', only: [:show] do
+  resources :form_actions, path: 'forms', only: [:index, :show] do
     resources :form_submissions, path: 's', only: [:create]
+  end
+
+  authenticated :user do
+    root 'form_actions#index', as: :authenticated_root
   end
 
   root 'high_voltage/pages#show', id: 'home'

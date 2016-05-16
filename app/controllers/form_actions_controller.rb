b2e58ec8c34f49a62a495417c7fe436dbccf9774
Kevin Attfield <k.attfield@gmail.com>
2016-05-15 17:33:18 -0700

    Add form for creating FormAction

--- app/controllers/form_actions_controller.rb
+++ app/controllers/form_actions_controller.rb
@@ -6,4 +6,14 @@ class FormActionsController < ApplicationController
   def show
     @form_action = FormAction.find(params[:id])
   end
+
+  def create
+    @form_action = current_user.form_actions.new(form_action_params)
+    @form_action.user_id = current_user.id if current_user
+    if @form_action.save
+      redirect_to @form_action
+    else
+      render "index"
+    end
+  end
 end

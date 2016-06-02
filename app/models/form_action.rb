b57558d478c7954af007c8b7963de917b3b43871
Graeme Nathan <graeme@stembolt.com>
2016-06-02 15:15:32 -0700

    Update partials, menu, constraints.
    Add login/registration menu partials
    
    Show debug params in dev env
    
    Move login button to menu
    
    Create error message partial
    
    Add unique constraint on (user_id, form name) pairs.

--- app/models/form_action.rb
+++ app/models/form_action.rb
@@ -1,5 +1,5 @@
 class FormAction < ActiveRecord::Base
   belongs_to :user
   has_many :form_submissions
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user_id }
 end

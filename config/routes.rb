84e2e628f8892930275504d598713a3466bd614e
Mike Conlin <mikeyconlin@gmail.com>
2016-06-13 12:42:56 -0700

    Add authentication to user in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :form_actions, path: 'forms', only: [:index, :show, :create] do
-    resources :form_submissions, path: 's', only: [:create]
+  authenticate :user do
+    resources :form_actions, path: 'forms' do
+      resources :form_submissions, path: 's', only: [:create]
+    end
   end
 
   authenticated :user do

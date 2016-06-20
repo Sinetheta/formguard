f8e54a1edd70b249532152b365dee242c4488635
Mike Conlin <mikeyconlin@gmail.com>
2016-06-20 16:15:39 -0700

    Display team name if form belongs to team

--- app/models/form_action.rb
+++ app/models/form_action.rb
@@ -3,4 +3,8 @@ class FormAction < ActiveRecord::Base
   belongs_to :team
   has_many :form_submissions
   validates :name, presence: true, uniqueness: { scope: :user_id }
+
+  def belongs_to
+    self.team_id ? Team.find(self.team_id).name : User.find(self.user_id).email
+  end
 end

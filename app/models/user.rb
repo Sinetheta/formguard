2f9ac5c9563b032a23f9339b729f86f78e074a52
Mike Conlin <mikeyconlin@gmail.com>
2016-06-27 15:12:45 -0700

    Add token to invite model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ActiveRecord::Base
   has_many :teams, through: :memberships
   has_many :invitations, class_name: 'Invite', foreign_key: 'recipient_id'
   has_many :sent_invites, class_name: 'Invite', foreign_key: 'sender_id'
+
+  attr_accessor :invite_token
 end

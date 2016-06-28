0bcab1e539029886ae9489dd210e6d0cce538a38
Graeme Nathan <graeme@stembolt.com>
2016-06-28 13:49:05 -0700

    Add webhookable to FormAction

--- app/models/form_action.rb
+++ app/models/form_action.rb
@@ -7,4 +7,11 @@ class FormAction < ActiveRecord::Base
   def belongs_to
     self.team_id ? Team.find(self.team_id).name : User.find(self.user_id).email
   end
+  has_many :web_hooks, as: :webhookable
+
+  include Webhookable
+
+  def web_hook_dispatcher
+    @dispatcher ||= Webhookable::Dispatcher.new(self)
+  end
 end

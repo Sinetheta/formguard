a58c8891e21f74ac2c99beb1b86775d0f29b6b60
Graeme Nathan <graeme@stembolt.com>
2016-07-05 16:59:58 -0700

    Enable user destruction of own forms

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,10 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    can :create, [FormAction, FormSubmission, WebHook, Team]
-    can :read, [FormAction, FormSubmission, WebHook], user_id: user.id
+    can :manage, FormAction, user_id: user.id
+    can :create, [FormSubmission, WebHook, Team]
+    can :read, [FormSubmission, WebHook], user_id: user.id
     can :read, [FormAction, FormSubmission, WebHook], team: { id: user.team_ids }
-    can :update, [FormAction], user_id: user.id
     can :read, Team do |t|
       user.team_ids.include? t.id
     end

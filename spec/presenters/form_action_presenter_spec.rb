88e5e03020279aecff282f29ab323647ddad666d
Graeme Nathan <graeme@stembolt.com>
2016-07-13 14:01:06 -0700

    Fix false positive spec warning

--- spec/presenters/form_action_presenter_spec.rb
+++ spec/presenters/form_action_presenter_spec.rb
@@ -16,11 +16,11 @@ RSpec.describe "FormActionPresenter" do
     subject { decorated_action.build_form_uri }
 
     it "uses valid compenents" do
-      expect{ subject }.not_to raise_error(URI::InvalidComponentError)
+      expect{ subject }.not_to raise_error
     end
 
     it "returns a valid URI" do
-      expect{ subject }.not_to raise_error(URI::InvalidURIError)
+      expect{ subject }.not_to raise_error
     end
 
   end

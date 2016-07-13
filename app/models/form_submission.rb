4ac40da71e9e58e8cd6737532b0329a8bc5b14eb
Graeme Nathan <graeme@stembolt.com>
2016-07-13 11:40:56 -0700

    Scope submissions by read/unread

--- app/models/form_submission.rb
+++ app/models/form_submission.rb
@@ -3,4 +3,6 @@ class FormSubmission < ActiveRecord::Base
   store_accessor :payload
   scope :until_date, -> (date) { where("created_at <= ?", date) }
   scope :from_date, -> (date) { where("created_at >= ?", date) }
+  scope :unread, -> { where(read: false) }
+  scope :read, -> { where(read: true) }
 end

74a8f678656a436709a668a04e7a7c900da05c8f
Mike Conlin <mikeyconlin@gmail.com>
2016-07-18 11:29:14 -0700

    Team owner check on memberships#destroy

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -29,10 +29,21 @@ RSpec.describe MembershipsController, type: :controller do
       context "and they try to kill themself" do
         it "doesn't let them kill themself" do
           delete :destroy,
-            id: admin.memberships.first.id,
-            team_id: admin.memberships.first.team_id
+            id: admin.memberships.first.id
           expect{ subject }.to change(team.members, :count).by(0)
         end
+      end
+    end
+
+    context "when current_user is team owner" do
+      before do 
+        allow(controller).to receive(:current_user) { member }
+        member.add_role(:owner, team)
+      end
+
+      it "warns user to transfer team ownership first" do
+        delete :destroy, id: member.memberships.first.id
+        expect(team.owner).to eq member
       end
     end
   end

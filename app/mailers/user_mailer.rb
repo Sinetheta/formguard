3d929f6812d0098ab1d2397fd595b8e43bf47c0e
Mike Conlin <mikeyconlin@gmail.com>
2016-07-19 09:14:08 -0700

    Wrap email message params in object for mailer view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,26 @@
 class UserMailer < ApplicationMailer
 
+  class SubmissionMessage
+    attr_reader :email, :action, :content, :user, :bcc_list, :payload
+
+    def initialize(submission)
+      @action = submission.form_action
+      @email = submission.payload["email"]
+      @content = submission.payload.except("email")
+      @user = @action.user
+      @bcc_list = @action.emails
+      @payload = submission.payload
+    end
+  end
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.submission_notification.subject
   #
+  
   def submission_notification(submission)
-    @submission = submission
-    @action = submission.form_action
-    @user = @action.user
-    @greeting = "Hi there,\n#{@user.email} asked that you be notified!"
-    mail(bcc: @action.emails, subject: "Submission notification")
+    @message = SubmissionMessage.new(submission)
+    mail(bcc: @message.bcc_list, subject: "Submission notification")
   end
 end

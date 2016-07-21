7a814a7d523dd08179c58be9c22a1af1db92f97b
Mike Conlin <mikeyconlin@gmail.com>
2016-07-21 10:27:04 -0700

    Invites controller specs

--- spec/controllers/invites_controller_spec.rb
+++ spec/controllers/invites_controller_spec.rb
@@ -6,39 +6,54 @@ RSpec.describe InvitesController, type: :controller do
   let(:team) { create(:team) }  
 
   describe "POST create" do
-    before { allow(controller).to receive(:current_user) { user } }
     subject(:create_invite) { post :create, invite: invite_params }
 
-    context "to a new user" do
-      context "with a valid email" do
-        let(:invite_params) { { team_id: team.id, email: "new_user@example.com" } }
-        
+    context "when user is an admin" do
+
+      before do 
+        user.add_role :admin, team
+        allow(controller).to receive(:current_user) { user } 
+      end
+
+      context "to a new user" do
+        context "with a valid email" do
+          let(:invite_params) { { team_id: team.id, email: "new_user@example.com" } }
+
+          it "creates an invite" do
+            expect{ create_invite }.to change(Invite, :count).by(1)
+          end
+        end
+
+        context "without a valid email" do
+          let(:invite_params) { { team_id: team.id, email: "invalid" } }
+
+          it "does not create an invite" do
+            expect{ create_invite }.to change(Invite, :count).by(0)
+          end
+        end
+      end
+
+      context "to an existing user" do
+        let(:invite_params) { { team_id: team.id, email: recipient.email } }
+
         it "creates an invite" do
           expect{ create_invite }.to change(Invite, :count).by(1)
         end
-      end
 
-      context "without a valid email" do
-        let(:invite_params) { { team_id: team.id, email: "invalid" } }
-
-        it "does not create an invite" do
-          expect{ create_invite }.to change(Invite, :count).by(0)
+        it "adds the user to the current_user's team" do
+          expect { create_invite }.to change(team.members, :count).by(1)
         end
       end
+    end 
 
-    end
+    context "when user is not admin" do
+      let(:invite_params) { { team_id: team.id, email: "new_user@example.com" } }
+      before{ allow(controller).to receive(:current_user) { user } }
 
-    context "to an existing user" do
-      let(:invite_params) { { team_id: team.id, email: recipient.email } }
-
-      it "creates an invite" do
-        expect{ create_invite }.to change(Invite, :count).by(1)
-      end
-
-      it "adds the user to the current_user's team" do
-        expect { create_invite }.to change(team.members, :count).by(1)
+      it "does not create an invite" do
+        expect{ create_invite }.to change(Invite, :count).by(0)
       end
     end
-  end 
+  end
 
 end

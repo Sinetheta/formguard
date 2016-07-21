befb1a1736a67f2e760fee8fedf4ef2f20efdf30
Mike Conlin <mikeyconlin@gmail.com>
2016-07-21 09:59:48 -0700

    Authorize #create in invite controller

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,16 +1,16 @@
 class InvitesController < ApplicationController
+  load_and_authorize_resource
 
   def create
-    invite = Invite.new(invite_params)
-    invite.sender_id = current_user.id
-    invite.add_recipient_to_invite_if_user_exists
-    if invite.save
-      invite.add_recipient_to_team
+    @invite.sender = current_user
+    @invite.add_recipient_to_invite_if_user_exists
+    if @invite.save
+      @invite.add_recipient_to_team
       flash[:notice] = "Invite sent"
     else
       flash[:error] = "Something went wrong"
     end
-    redirect_to team_path(invite.team)
+    redirect_to team_path(@invite.team)
   end
 
   def invite_params

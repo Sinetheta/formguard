ff8730a668236425ea3399b36ebf4f36486f5334
Graeme Nathan <graeme@stembolt.com>
2016-07-27 17:13:50 -0700

    Remove load_and_authorize_resource

--- app/controllers/form_submissions_controller.rb
+++ app/controllers/form_submissions_controller.rb
@@ -1,10 +1,11 @@
 class FormSubmissionsController < ApplicationController
   protect_from_forgery with: :null_session
-  load_and_authorize_resource
+
+  before_action :load_form_action
 
   def create
-    form_action = FormAction.find(params[:form_action_id])
-    submission = form_action.form_submissions.new(payload: payload)
+    submission = @form_action.form_submissions.new(payload: payload)
+    authorize! :create, FormSubmission
     file = attachment
     if file
       submission.attachment = file.read
@@ -12,9 +13,9 @@ class FormSubmissionsController < ApplicationController
       submission.attachment_type = file.content_type
     end
     if submission.save
-      UserMailer.submission_notification(submission).deliver_now if form_action.should_notify?
-      AutoResponseMailer.submission_notification(form_action, submission).deliver_now if form_action.auto_response
-      form_action.web_hook_dispatcher.deliver(:submission, payload.to_json)
+      UserMailer.submission_notification(submission).deliver_now if @form_action.should_notify?
+      AutoResponseMailer.submission_notification(@form_action, submission).deliver_now if @form_action.auto_response
+      @form_action.web_hook_dispatcher.deliver(:submission, payload.to_json)
       head 200
     else
       head 500
@@ -22,17 +23,29 @@ class FormSubmissionsController < ApplicationController
   end
 
   def update
+    load_form_submission
+    authorize! :update, @form_submission
     @form_submission.update_attribute(:read, true)
     render :show
   end
 
   def download_attachment
+    load_form_submission
+    authorize! :download_attachment, @form_submission
     send_data(@form_submission.attachment,
               type: @form_submission.attachment_type,
               name: @form_submission.attachment_name)
   end
 
   private
+
+  def load_form_action
+    @form_action = FormAction.find(params[:form_action_id])
+  end
+
+  def load_form_submission
+    @form_submission = FormSubmission.find(params[:id])
+  end
 
   def payload
     params.except(:controller,

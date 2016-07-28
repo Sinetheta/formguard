4ac2c530ff072728be0cee124c689db2b30418de
Graeme Nathan <graeme@stembolt.com>
2016-07-27 17:12:12 -0700

    Fix routes for uniformity

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   resources :form_actions, path: 'forms' do
-    resources :form_submissions, path: 's', only: [:create, :new]
+    resources :form_submissions, path: 's', only: [:create, :new, :update]
+    get '/s/:id/download' => "form_submissions#download_attachment"
     resources :web_hooks
   end
 
-  resources :form_submissions, path: 'submissions', only: [:update]
+  get '/forms/:id/filtered_submissions' => "form_actions#render_filtered_partial"
+
   resources :memberships, only: :destroy, as: 'delete_team_membership'
 
   resources :teams do
@@ -27,7 +29,6 @@ Rails.application.routes.draw do
 
   root 'high_voltage/pages#show', id: 'home'
 
-  get 'submissions/:id/download' => "form_submissions#download_attachment"
   get 'forms/:id/filtered_submissions' => "form_actions#render_filtered_partial"
 
 end

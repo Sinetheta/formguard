bfff3584d6402fb92677245de01278c3690b8004
Kevin Attfield <kevin@stembolt.com>
2017-01-28 16:38:22 -0800

    Don't bother with dynamic port

--- app/presenters/form_action_presenter.rb
+++ app/presenters/form_action_presenter.rb
@@ -4,14 +4,7 @@ class FormActionPresenter < BasePresenter
   end
 
   def build_form_uri
-    form_uri = URI::HTTP.build({host: ENV['APPLICATION_HOST'], path: action_path})
-    # heroku sets a PORT automatically, it's an internal port that we don't want
-    # so here we just assume that outside of dev the app is always on 80 or 443
-    # both of which can be safely omitted
-
-    ENV["USE_SERVER_PORT"] && form_uri.port = Rails::Server.new.options[:Port]
-
-    form_uri
+    URI::HTTP.build({host: ENV['APPLICATION_HOST'], path: action_path})
   end
 
   private
